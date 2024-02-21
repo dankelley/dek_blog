d06b706f2463958a1e48176a478de722f344873f
dankelley <kelley.dan@gmail.com>
2024-02-21 10:47:46 -0400

    cleanup

--- R/amsr-maritime.R
+++ R/amsr-maritime.R
@@ -2,11 +2,11 @@ library(oce)
 library(dod)
 f <- dod.amsr()
 a <- read.amsr(f)
-png("2024-02-21-cruise-temperature.png",
+png("amsr-maritime.png",
     unit = "in",
     width = 7, height = 4, res = 200, pointsize = 9
 )
-plot(a, xlim = c(-100, -50), ylim = c(12, 30), col = oceColorsTurbo)
+plot(a, xlim = c(-70, -40), ylim = c(35, 50), col = oceColorsTurbo)
 data(coastlineWorldFine, package="ocedata")
 lines(coastlineWorldFine[["longitude"]], coastlineWorldFine[["latitude"]])
 dev.off()
